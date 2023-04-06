96fff9aa85480affe69b6ecd37ff6c467c2c2aa3
Drashti Kansare <drashtik.inexture@gmail.com>
2023-04-06 15:14:18 +0530

    fix: f- Fix Agency Dashboard design

--- src/helper/images.ts
+++ src/helper/images.ts
@@ -2,10 +2,12 @@ import Logo from "../assets/images/logonew.svg";
 import UserAvatar from "../assets/images/avataruser.png";
 import UserProfile from "../assets/images/profile.png";
 import LogoutPopupVector from "../assets/images/logoutPopupImage.png";
+import Sort from "../assets/images/Sort.png";
 
 export const Images = {
   Logo,
   UserAvatar,
   UserProfile,
-  LogoutPopupVector
+  LogoutPopupVector,
+  Sort
 };
