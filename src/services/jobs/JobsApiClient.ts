584e105cab1ea9f16bc309e0a0d3f3d6a7d8a58b
Drashti-711 <drashtikansare@gmail.com>
2023-04-14 15:35:06 +0530

    feat: f- Design and implement Admin Add Edit Job Page

--- src/services/jobs/JobsApiClient.ts
+++ src/services/jobs/JobsApiClient.ts
@@ -2,15 +2,18 @@ import axiosPrivate from "../../api/axios";
 import { API_URL } from "../../helper/env";
 
 class JobsApiClient {
+  fetchAllJobsList = (data) =>
+    axiosPrivate.post(`${API_URL.MY_JOBS.AGENCY_JOBS_LIST}/` + data);
 
-  fetchAllJobsList = (data) => 
-  axiosPrivate.post(`${API_URL.MY_JOBS.AGENCY_JOBS_LIST}/` + data);
-  
   fetchJobsDetails = (id) =>
     axiosPrivate.get(`${API_URL.HOMEPAGE.JOBS_LIST}${id}/`);
 
   jobApply = (formData) =>
     axiosPrivate.post(`${API_URL.HOMEPAGE.JOB_APPLIED}/` + formData);
+
+  //create job
+  createApply = (formData) =>
+    axiosPrivate.post(`${API_URL.HOMEPAGE.JOBS_LIST}` ,formData);
 
   //delete job
   deleteJob = (id) =>
