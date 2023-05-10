d555fbb97873ea1e808b3a1f334f641060589585
JinitaINX <jinita.inexture@gmail.com>
2023-05-10 16:55:04 +0530

    Story details meta tag

--- src/components/pages/agency/companyProjects/AgencyCompanyProjectsDetails.tsx
+++ src/components/pages/agency/companyProjects/AgencyCompanyProjectsDetails.tsx
@@ -1,147 +1,179 @@
-import React from 'react';
-import { Box, Card, Grid, Typography } from '@mui/material';
-import { useParams } from 'react-router-dom';
-import { formateISODateToLocaleString } from 'helper/utility/customFunctions';
-import { useAppSelector } from 'redux/store';
-import { COMPANY_PROJECTS, COMPANY_PROJECTS_DATA, COMPANY_PROJECTS_FILTERS_DATA } from 'redux/reducers/companies/companies.slice';
-
+import React from "react";
+import { Box, Card, Grid, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { formateISODateToLocaleString } from "helper/utility/customFunctions";
+import { useAppSelector } from "redux/store";
+import {
+  COMPANY_PROJECTS,
+  COMPANY_PROJECTS_DATA,
+  COMPANY_PROJECTS_FILTERS_DATA,
+} from "redux/reducers/companies/companies.slice";
+import { Helmet } from "react-helmet-async";
 
 const AgencyCompanyProjectsDetails = () => {
+  const { communityId } = useParams();
 
-    const { communityId } = useParams();
+  // const { agencyCompanyProjectsList } = useSelector((state) => state.AgencyCompanyProjectsReducer);
+  const companyProjectsList = useAppSelector(COMPANY_PROJECTS_DATA);
+  const companyProjectsFilters = useAppSelector(COMPANY_PROJECTS_FILTERS_DATA);
 
-    // const { agencyCompanyProjectsList } = useSelector((state) => state.AgencyCompanyProjectsReducer);
-    const companyProjectsList = useAppSelector(COMPANY_PROJECTS_DATA);
-    const companyProjectsFilters = useAppSelector(COMPANY_PROJECTS_FILTERS_DATA);
+  // useEffect(() => {
+  //     const details = agencyCompanyProjectsList.filter((e) => e.id == communityId);
 
-    // useEffect(() => {
-    //     const details = agencyCompanyProjectsList.filter((e) => e.id == communityId);
+  //     console.log({ agencyCompanyProjectsList, communityId, details }, 'Details');
 
-    //     console.log({ agencyCompanyProjectsList, communityId, details }, 'Details');
+  //     setSelectedComunityDetails(details);
+  // }, [agencyCompanyProjectsList, communityId]);
 
-    //     setSelectedComunityDetails(details);
-    // }, [agencyCompanyProjectsList, communityId]);
+  // // const selectedComunityDetails = useCallback(() => agencyCompanyProjectsList.find((e) => e.id === communityId), [communityId, agencyCompanyProjectsList]);
 
-    // // const selectedComunityDetails = useCallback(() => agencyCompanyProjectsList.find((e) => e.id === communityId), [communityId, agencyCompanyProjectsList]);
+  // console.log(selectedComunityDetails);
 
-    // console.log(selectedComunityDetails);
+  return (
+    <div className="page-container">
+      <h1 className="page-title">Story Details</h1>
 
+      <div className="page-card card p-0">
+        {companyProjectsList.data.results?.filter((e) => e.id == communityId)
+          ?.length > 0 &&
+          companyProjectsList.data.results
+            ?.filter((e) => e.id == communityId)
+            ?.map((item) => (
+              <Box
+                sx={{ width: "100%", py: 2, pl: 2, pr: 3.125 }}
+                key={item.id}
+              >
+                <Helmet>
+                  <title>{item?.title}</title>
+                  <meta property="og:locale" content="en_US" />
+                  <link rel="canonical" href={window.location.origin} />
+                  <meta name="description" content={item?.lede} />
+                  {/* <meta property="og:title" content="Adifect Frontend title" />
+        <meta
+          property="og:description"
+          content="Description Adifect-Job portal"
+        /> */}
+                  <meta property="og:type" content="website" />
+                  <meta property="og:site_name" content="Job Portal" />
+                  <meta property="og:url" content={window.location.href} />
+                  <meta property="og:image" content={item?.image[0]} />
+                  <meta
+                    property="og:image:secure_url"
+                    content={item?.image[0]}
+                  />
+                </Helmet>
+                <Grid container>
+                  <Grid xs={8} p={2} pr={6}>
+                    <Typography variant="h4">{item.title}</Typography>
 
-    return (
-        <div className="page-container">
-            <h1 className='page-title'>Story Details</h1>
-
-            <div className="page-card card p-0">
-                {(companyProjectsList.data.results?.filter((e) => e.id == communityId)?.length > 0) &&
-                    companyProjectsList.data.results?.filter((e) => e.id == communityId)?.map((item) => (
-                        <Box sx={{ width: '100%', py: 2, pl: 2, pr: 3.125 }} key={item.id}>
-                            <Grid container>
-                                <Grid xs={8} p={2} pr={6}>
-                                    <Typography variant='h4'>
-                                        {item.title}
-                                    </Typography>
-
-                                    <Typography my={2} sx={{ textAlign: 'justify' }}>
-                                        {item.lede}
-                                    </Typography>
-                                    {item.image &&
-                                        <Box
-                                            sx={{
-                                                display: 'flex',
-                                                overflow: 'hidden',
-                                                width: '100%',
-                                                borderRadius: '1rem',
-                                                "& img": {
-                                                    objectFit: 'cover',
-                                                }
-                                            }}>
-                                            <img src={item.image} width="100%" height="100%" />
-                                        </Box>}
-                                    <Box mt={3} sx={{
-                                        "& p": {
-                                            marginBottom: '30px',
-                                        }
-                                    }}>
-                                        <Typography sx={{ textAlign: 'justify' }} dangerouslySetInnerHTML={{ __html: item.body }} />
-                                    </Box>
-                                </Grid>
-                                <Grid xs={4}>
-                                    <Card
-                                        sx={{
-                                            maxWidth: '689px',
-                                            width: '100%',
-                                            position: 'relative',
-                                            overflow: 'unset',
-                                            display: 'flex',
-                                            p: 1.875,
-                                            borderRadius: 0.5,
-                                            "&.MuiPaper-root": {
-                                                "&.MuiPaper-elevation": {
-                                                    "&.MuiPaper-rounded": {
-                                                        "&.MuiPaper-elevation1": {
-                                                            // boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
-                                                            boxShadow: 'none',
-                                                            border: '1px solid rgba(113, 117, 123, 0.2);',
-                                                            // filter: 'drop-shadow(rgba(0, 0, 0, 0.25) 0px 0px 4px)',
-                                                            m: 0,
-                                                            mb: 2,
-                                                        }
-                                                    }
-                                                }
-                                            },
-                                        }}>
-                                        <Grid container>
-                                            <Grid xs={12}>
-                                                <Typography
-                                                    sx={{ color: '#71757B' }}
-                                                >
-                                                    Community
-                                                </Typography>
-                                                <Typography
-                                                    sx={{
-                                                        fontFamily: '"Figtree", sans-serif',
-                                                        fontWeight: 500,
-                                                        lineHeight: '22px',
-                                                    }}
-                                                >
-                                                    {item.community.name ?? ''}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid xs={12} mt={2}>
-                                                <Typography
-                                                    sx={{ color: '#71757B' }}
-                                                >
-                                                    Published Date
-                                                </Typography>
-                                                <Typography
-                                                    sx={{
-                                                        fontFamily: '"Figtree", sans-serif',
-                                                        fontWeight: 500,
-                                                        lineHeight: '22px',
-                                                    }}
-                                                >
-                                                    {formateISODateToLocaleString(item.story_metadata.published_at) ?? ''}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid xs={12} mt={1}>
-                                                <Typography
-                                                    sx={{ color: '#71757B' }}
-                                                >
-                                                    Updated Date
-                                                </Typography>
-                                                <Typography
-                                                    sx={{
-                                                        fontFamily: '"Figtree", sans-serif',
-                                                        fontWeight: 500,
-                                                        lineHeight: '22px',
-                                                    }}
-                                                >
-                                                    {formateISODateToLocaleString(item.story_metadata.updated_at) ?? ''}
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </Card>
-                                    {/* <Card
+                    <Typography my={2} sx={{ textAlign: "justify" }}>
+                      {item.lede}
+                    </Typography>
+                    {item.image && (
+                      <Box
+                        sx={{
+                          display: "flex",
+                          overflow: "hidden",
+                          width: "100%",
+                          borderRadius: "1rem",
+                          "& img": {
+                            objectFit: "cover",
+                          },
+                        }}
+                      >
+                        <img src={item.image[0]} width="100%" height="100%" />
+                      </Box>
+                    )}
+                    <Box
+                      mt={3}
+                      sx={{
+                        "& p": {
+                          marginBottom: "30px",
+                        },
+                      }}
+                    >
+                      <Typography
+                        sx={{ textAlign: "justify" }}
+                        dangerouslySetInnerHTML={{ __html: item.body }}
+                      />
+                    </Box>
+                  </Grid>
+                  <Grid xs={4}>
+                    <Card
+                      sx={{
+                        maxWidth: "689px",
+                        width: "100%",
+                        position: "relative",
+                        overflow: "unset",
+                        display: "flex",
+                        p: 1.875,
+                        borderRadius: 0.5,
+                        "&.MuiPaper-root": {
+                          "&.MuiPaper-elevation": {
+                            "&.MuiPaper-rounded": {
+                              "&.MuiPaper-elevation1": {
+                                // boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
+                                boxShadow: "none",
+                                border: "1px solid rgba(113, 117, 123, 0.2);",
+                                // filter: 'drop-shadow(rgba(0, 0, 0, 0.25) 0px 0px 4px)',
+                                m: 0,
+                                mb: 2,
+                              },
+                            },
+                          },
+                        },
+                      }}
+                    >
+                      <Grid container>
+                        <Grid xs={12}>
+                          <Typography sx={{ color: "#71757B" }}>
+                            Community
+                          </Typography>
+                          <Typography
+                            sx={{
+                              fontFamily: '"Figtree", sans-serif',
+                              fontWeight: 500,
+                              lineHeight: "22px",
+                            }}
+                          >
+                            {item.community.name ?? ""}
+                          </Typography>
+                        </Grid>
+                        <Grid xs={12} mt={2}>
+                          <Typography sx={{ color: "#71757B" }}>
+                            Published Date
+                          </Typography>
+                          <Typography
+                            sx={{
+                              fontFamily: '"Figtree", sans-serif',
+                              fontWeight: 500,
+                              lineHeight: "22px",
+                            }}
+                          >
+                            {formateISODateToLocaleString(
+                              item.story_metadata.published_at
+                            ) ?? ""}
+                          </Typography>
+                        </Grid>
+                        <Grid xs={12} mt={1}>
+                          <Typography sx={{ color: "#71757B" }}>
+                            Updated Date
+                          </Typography>
+                          <Typography
+                            sx={{
+                              fontFamily: '"Figtree", sans-serif',
+                              fontWeight: 500,
+                              lineHeight: "22px",
+                            }}
+                          >
+                            {formateISODateToLocaleString(
+                              item.story_metadata.updated_at
+                            ) ?? ""}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Card>
+                    {/* <Card
                                             sx={{
                                                 maxWidth: '689px',
                                                 width: '100%',
@@ -208,13 +240,13 @@ const AgencyCompanyProjectsDetails = () => {
                                                 </Grid>
                                             </Grid>
                                         </Card> */}
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    ))}
-            </div>
-        </div>
-    )
-}
+                  </Grid>
+                </Grid>
+              </Box>
+            ))}
+      </div>
+    </div>
+  );
+};
 
 export default AgencyCompanyProjectsDetails;
